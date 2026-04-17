fc5929d2d7aa94de3fcced95747b039e41af405e
“zhy0504” <490630293@qq.com>
2026-04-17 16:05:25 +0800

    Initialize H5 application scaffold
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Intro from '@/views/Intro.vue'
+import Quiz from '@/views/Quiz.vue'
+import Result from '@/views/Result.vue'
+import Knowledge from '@/views/Knowledge.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/intro',
+    name: 'Intro',
+    component: Intro
+  },
+  {
+    path: '/quiz',
+    name: 'Quiz',
+    component: Quiz
+  },
+  {
+    path: '/result',
+    name: 'Result',
+    component: Result
+  },
+  {
+    path: '/knowledge',
+    name: 'Knowledge',
+    component: Knowledge
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
